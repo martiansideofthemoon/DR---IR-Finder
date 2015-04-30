421b4a648e40b469ec8db6c25c213458764c89ae
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-05-01 04:10:42 +0530

    Year and Department drop down lists provided

--- drirfinder/forms.py
+++ drirfinder/forms.py
@@ -2,14 +2,13 @@ from django import forms
 from drirfinder.models import Category
 
 class CategoryForm(forms.ModelForm):
-    roll_number = forms.CharField(max_length=128, help_text="Roll Number : ")
-    cpi = forms.DecimalField(max_digits=3, decimal_places=2, help_text="CPI : ")
-    department = forms.CharField(max_length=128 , help_text="Department : ");
-
-    # An inline class to provide additional information on the form.
-    class Meta:
-        # Provide an association between the ModelForm and a model
-        model = Category
-        fields = ('roll_number','cpi','department')
+	roll_number = forms.CharField(max_length=128, help_text="Roll Number : ")
+	year = forms.CharField(widget=forms.Select(choices=Category.YEARS), help_text="Year: ");
+	cpi = forms.DecimalField(max_digits=3, decimal_places=2, help_text="CPI : ")
+	department = forms.CharField(widget=forms.Select(choices=Category.DEPARTMENTS), help_text="Deparment: ");
+	
+	class Meta:
+		model = Category
+		fields = ('roll_number','year','cpi','department')
 
 
--- drirfinder/models.py
+++ drirfinder/models.py
@@ -2,10 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    roll_number = models.CharField(max_length=128, unique=True, default="0")
-    cpi = models.DecimalField( max_digits=3, decimal_places=2, default=0.0)
-    department = models.CharField(max_length=128, default="null")
-
-    def __unicode__(self):  #For Python 2, use __str__ on Python 3
-        return self.roll_number
+	DEPARTMENTS = (
+		('CSE' , 'Computer Science and Engineering'),
+		('EE' , 'Electrical Engineering'),
+		('ME' , 'Mechanical Engineering'),
+		('MEMS' , 'Metallurgy and Material Science'),
+		('CE' , 'Civil Engineering'),
+		('CL' , 'Chemical Engineering'),
+		('EP' , 'Engineering Physics'),
+		('ESE' , 'Energy Science and Engineering'),
+		('AE' , 'Aerospace Engineering'),
+		('CH' , 'Chemistry'),
+		)
+	YEARS = (
+		('1' , 'Freshie'),
+		('2' , 'Sophie'),
+		('3' , 'Thirdie'),
+		('4' , 'Fourthie'),
+		)
+	roll_number = models.CharField(max_length=128, unique=True, default="0")
+	cpi = models.DecimalField( max_digits=3, decimal_places=2, default=0.0)
+	department = models.CharField(max_length=4, choices=DEPARTMENTS)
+	year = models.CharField(max_length=1, choices=YEARS)
+	def __unicode__(self):
+		return self.roll_number
 
